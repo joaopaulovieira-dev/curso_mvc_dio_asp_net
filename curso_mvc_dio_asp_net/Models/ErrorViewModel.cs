1b74fc09dd36c2351163bc1b9b4d16a05625d441
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-09-12 15:15:29 -0300

    Primeiro Commit

---
+++ curso_mvc_dio_asp_net/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace curso_mvc_dio_asp_net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
