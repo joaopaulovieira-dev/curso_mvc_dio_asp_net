773157a12dc1ec7dd95c5a63fcb10c75a44f2d25
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-09-15 22:13:52 -0300

    Aplicando novas categorias no projeto

--- curso_mvc_dio_asp_net/Models/Categoria.cs
+++ curso_mvc_dio_asp_net/Models/Categoria.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace curso_mvc_dio_asp_net.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
         public string Descricao { get; set; }
 
         public List<Produto> Produtos { get; set; }
--- curso_mvc_dio_asp_net/Models/Produto.cs
+++ curso_mvc_dio_asp_net/Models/Produto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace curso_mvc_dio_asp_net.Models
 {
     public class Produto
     {
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
         public string Descrição { get; set; }
+        [Range(1, 10, ErrorMessage = "Valor fora da faixa.")]
         public int Quantidade { get; set; }
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
